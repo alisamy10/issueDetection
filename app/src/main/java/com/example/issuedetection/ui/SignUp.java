b6f611a79959ee15797df659a5c463eb9a21814c
Muhammed <ma2078941@gmail.com>
2020-01-27 21:48:44 +0200

    splash

--- app/src/main/java/com/example/issuedetection/ui/SignUp.java
+++ app/src/main/java/com/example/issuedetection/ui/SignUp.java
@@ -3,10 +3,12 @@ package com.example.issuedetection.ui;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.issuedetection.Base.BaseActivity;
 import com.example.issuedetection.R;
 
-public class SignUp extends BaseActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/issuedetection/ui/Splash.java
@@ -0,0 +1,28 @@
+package com.example.issuedetection.ui;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.issuedetection.Base.BaseActivity;
+import com.example.issuedetection.R;
+
+public class Splash extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
