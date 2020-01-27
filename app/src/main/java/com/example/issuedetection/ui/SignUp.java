2367c6438041c48fbe0c2c6cfe019b6e2d3c7b4c
Muhammed <ma2078941@gmail.com>
2020-01-27 21:24:10 +0200

    add some data2

---
+++ app/src/main/java/com/example/issuedetection/ui/SignUp.java
@@ -0,0 +1,16 @@
+package com.example.issuedetection.ui;
+
+
+import android.os.Bundle;
+
+import com.example.issuedetection.Base.BaseActivity;
+import com.example.issuedetection.R;
+
+public class SignUp extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
